9496d21f70d524e4697847169e8e77c3ceb42eb0
jay <jay@pdd.com>
2022-06-16 20:31:10 +0800

    first commit

---
+++ src/main/java/com/atpdd/com/PddTest.java
@@ -0,0 +1,11 @@
+package com.atpdd.com;
+
+/**
+ * @author hhzhang
+ * @creat 2022-06-16 20:29
+ */
+public class PddTest {
+    public static void main(String[] args) {
+        System.out.println("我在写java");
+    }
+}
